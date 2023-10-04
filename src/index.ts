f9573616c01928100583e271fe60f8132940104b
Apichat Eakwongsa <apichat.eak@cjexpress.co.th>
2023-10-04 15:54:07 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { ThemeProvider } from 'styled-components'
+import ButtonDS from './components/ButtonDs'
+import { GlobalStyles } from './styles/global-style'
+import { crmTheme } from './styles/themes'
+
+export { ButtonDS, GlobalStyles, crmTheme, ThemeProvider }
