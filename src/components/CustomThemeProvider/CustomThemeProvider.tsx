e80332c105985811a6689997fc0c3f34d76d18f2
Apichat Eakwongsa <apichat.eak@cjexpress.co.th>
2023-10-06 14:28:37 +0700

    rolback v7

--- src/components/CustomThemeProvider/CustomThemeProvider.tsx
+++ src/components/CustomThemeProvider/CustomThemeProvider.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import crmTheme from '../../themes/themes'
-import { ThemeProvider } from 'styled-components'
+// import { ThemeProvider } from 'styled-components'
 // import GlobalStyles from '../../styles/global-style'
 
 interface CustomThemeProviderProps {
@@ -9,10 +9,12 @@ interface CustomThemeProviderProps {
 }
 
 const CustomThemeProvider = ({ theme = crmTheme, children }: CustomThemeProviderProps) => (
-  <ThemeProvider theme={theme}>
-    {/* <GlobalStyles /> */}
-    {children}
-  </ThemeProvider>
+  <>
+    {/* <ThemeProvider theme={theme}> */}
+      {/* <GlobalStyles /> */}
+      {children}
+    {/* </ThemeProvider> */}
+  </>
 )
 
 export default CustomThemeProvider
